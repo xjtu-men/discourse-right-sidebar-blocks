d1d23c25da005101cd05fdaf2f66ba9441c061b8
xjtu-men <136139347+xjtu-men@users.noreply.github.com>
2023-09-19 14:21:13 +0800

    save

--- javascripts/discourse/components/category-topics.js
+++ javascripts/discourse/components/category-topics.js
@@ -19,26 +19,23 @@ export default class CategoryTopics extends Component {
       return;
     }
 
-    const filter = "c/" + categoryId;
-    this.category = Category.findById(categoryId);
+    const filter = {
+      filter: "latest",
+      params: {
+        category: categoryId,
+      },
+    };
     this.store.set("isLoading", true);
-
-    this.store.findFiltered("topicList", { filter }).then((result) => {
-
-      const results = result.topic_list.topics;
-
-      results.forEach((topic) => {
-        topic.url = `${getURL("/t/")}${topic.slug}/${topic.id}`;
-        if (topic.last_read_post_number > 0) {
-          topic.url += `/${topic.last_read_post_number}`;
-        }
-      });
-
-      this.topics = results.slice(0, count);
+    this.store.findFiltered("topicList", { filter }).then((topicList) => {
+      this.topicList = topicList.topics.slice(0, count);
+      // results.forEach((topic) => {
+      //   topic.url = `${getURL("/t/")}${topic.slug}/${topic.id}`;
+      //   if (topic.last_read_post_number > 0) {
+      //     topic.url += `/${topic.last_read_post_number}`;
+      //   }
+      // });
       this.store.set("isLoading", false);
-
     });
-
   }
 
   willDestroy() {
